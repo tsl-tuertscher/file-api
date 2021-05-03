66c702c147e47bd111cc0dc55c20a7848a011b33
Stefan Tuertscher <stefan@tsl-tuertscher.com>
2021-05-03 14:22:57 +0200

    fs

--- fs_test.go
+++ fs_test.go
@@ -5,8 +5,30 @@ import (
 )
 
 func TestFileExists(t *testing.T) {
-    got := FileExists("./test.json")
-    if got == true{
-        t.Errorf("fileExists('./test.json')")
-    }
+	got := FileExists("./test.json")
+	if got == true {
+		t.Errorf("FileExists('./test.json')")
+	}
+}
+
+func TestGetMimeTypeFromFileType(t *testing.T) {
+	got := GetMimeTypeFromFileType("hgt")
+	if got != "application/octet-stream" {
+		t.Errorf("GetMimeTypeFromFileType('hgt')")
+	}
+
+	got = GetMimeTypeFromFileType("json")
+	if got != "application/json" {
+		t.Errorf("FileExists('json')")
+	}
+}
+
+func TestGetTileUrl(t *testing.T) {
+	var result Config
+    result.Offset = ""
+      
+	got := GetTileUrl(result, "test", "3", "3", "4", "json")
+    if got != "/test/default.json" {
+		t.Errorf("GetTileUrl()")
+	}
 }
